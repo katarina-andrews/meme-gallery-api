45a798aa8ed9533a002ccfe5776f31889ca86056
katarina-andrews <andrews.katarina@gmail.com>
2025-10-07 13:28:46 -0500

    added notes and imported get all user memes route

--- routes/memeRoutes.js
+++ routes/memeRoutes.js
@@ -5,14 +5,28 @@ import {
   createMeme,
   updateMeme,
   deleteMeme,
+  getUserMeme,
 } from "../controllers/memeController.js";
 
+// router to hold all memes routes 
 const router = express.Router();
 
+// route to get all memes
 router.get("/", getMemes);
+
+// route to get a meme by id
 router.get("/:id", getMemeById);
+
+// route to get all user memes
+router.get("/users/:id/memes", getUserMeme);
+
+// route to add a meme
 router.post("/", createMeme);
+
+// route to update a meme by id
 router.put("/:id", updateMeme);
+
+// route to delete a meme by id
 router.delete("/:id", deleteMeme);
 
 export default router;
