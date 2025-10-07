b94cc6471d49f4a21881a1a52d8cc6503688df8b
katarina-andrews <andrews.katarina@gmail.com>
2025-10-07 13:22:55 -0500

    added notes, 404 error handler, and root route

--- index.js
+++ index.js
@@ -3,21 +3,38 @@ import memeRoutes from "./routes/memeRoutes.js";
 const app = express();
 const PORT = 3000;
 
+// middleware to parse JSON bodies
 app.use(express.json());
 
+// middleware for logging
 function logger(req, res, next) {
   console.log(`${req.method} ${req.url} at ${new Date().toISOString()}`);
   next();
 }
 app.use(logger);
 
+// root route i.e homepage 
+app.get("/", (req, res) => {
+  response.send("Welcome to the Meme Gallery API");
+});
+
+// memes routes
 app.use("/memes", memeRoutes);
 
+// 404 error handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "We could not find the url you are looking for",
+    message: `route ${req.originalUrl} not found`,
+  });
+});
+
+// general error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: "Something went wrong!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Dev Meme API listening on port http://localhost:${PORT}`);
 });
