faab8f4c30d1717572e2a58da091d14c19261599
katarina-andrews <andrews.katarina@gmail.com>
2025-10-08 16:05:41 -0500

    added authenticate user middleware and protected the post meme route

--- routes/memeRoutes.js
+++ routes/memeRoutes.js
@@ -5,11 +5,25 @@ import {
   createMeme,
   updateMeme,
   deleteMeme,
-  getUserMeme,
 } from "../controllers/memeController.js";
+import { getUserMeme } from "../controllers/userController.js";
+import jwt from "jsonwebtoken";
 
-// router to hold all memes routes 
+// router to hold all memes routes
 const router = express.Router();
+
+// middleware to authenticate user
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, "secretkey", (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    req.user = decoded;
+    next();
+  });
+}
 
 // route to get all memes
 router.get("/", getMemes);
@@ -21,7 +35,7 @@ router.get("/:id", getMemeById);
 router.get("/users/:id/memes", getUserMeme);
 
 // route to add a meme
-router.post("/", createMeme);
+router.post("/", authenticateToken, createMeme);
 
 // route to update a meme by id
 router.put("/:id", updateMeme);
